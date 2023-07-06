8c9ac2be84571f0d198a180fd89eb1772ffe2fab
Craig Rupp <craigtrupp8@gmail.com>
2023-07-06 11:39:47 -0500

    Dictionary, List Comprehension and mapping of function calls shared across functions

---
+++ PythonProgramming/Procedural_Functional_OOP_Wk3/wk3_first_assessment.py
@@ -0,0 +1,96 @@
+# Input data: List of dictionaries
+employee_list = [
+   {"id": 12345, "name": "John", "department": "Kitchen"},
+   {"id": 12456, "name": "Paul", "department": "House Floor"},
+   {"id": 12478, "name": "Sarah", "department": "Management"},
+   {"id": 12434, "name": "Lisa", "department": "Cold Storage"},
+   {"id": 12483, "name": "Ryan", "department": "Inventory Mgmt"},
+   {"id": 12419, "name": "Gill", "department": "Cashier"}
+]
+
+# Function to be passed to the map() function. Do not change this.
+def mod(employee_list):
+   temp = employee_list['name'] + "_" + employee_list["department"]
+   return temp
+
+def to_mod_list(employee_list):
+   """ Modifies the employee list of dictionaries into list of employee-department strings
+
+   [IMPLEMENT ME] 
+      1. Use the map() method to apply mod() to all elements in employee_list
+
+   Args:
+      employee_list: list of employee objects
+
+   Returns:
+      list - A list of strings consisting of name + department.
+   """
+   ### WRITE SOLUTION CODE HERE
+   map_return = list(map(mod, employee_list))
+   return map_return
+   raise NotImplementedError()
+
+def generate_usernames(mod_list):
+   """ Generates a list of usernames 
+
+   [IMPLEMENT ME] 
+      1. Use list comprehension and the replace() function to replace space
+         characters with underscores
+
+      List comprehension looks like:
+      list = [ function() for <item> in <list> ]
+
+      The format for the replace() function is:
+
+      test_str.replace(“a”, “z”) # replaces every “a” in test_str with “z”
+
+   Args:
+      mod_list: list of employee-department strings
+
+   Returns:
+      list - A list of usernames consisting of name + department delimited by underscores.
+   """
+   ### WRITE SOLUTION CODE HERE
+   return [x.replace(' ', '_') for x in mod_list]
+   raise NotImplementedError()
+
+def map_id_to_initial(employee_list):
+   """ Maps employee id to first initial
+
+   [IMPLEMENT ME] 
+      1. Use dictionary comprehension to map each employee's id (value) to the first letter in their name (key)
+      2. create a dictionary where each key is the first letter of an employee's name and the value is the employee's ID.
+
+      Dictionary comprehension looks like:
+      dict = { key : value for <item> in <list> }
+
+   Args:
+      employee_list: list of employee objects
+
+   Returns:
+      dict - A dictionary mapping an employee's id (value) to their first initial (key).
+   """
+   ### WRITE SOLUTION CODE HERE
+   return {employee['name'][0]: employee['id'] for employee in employee_list}
+   raise NotImplementedError()
+
+def main():
+   mod_emp_list = to_mod_list(employee_list)
+   print("Modified employee list: " + str(mod_emp_list) + "\n")
+   # notice the function call below being able to be transcribed in to a f string
+   print(f"List of usernames: {generate_usernames(mod_emp_list)}\n")
+
+   print(f"Initials and ids: {map_id_to_initial(employee_list)}")
+
+if __name__ == "__main__":
+   main()
+
+
+# Outputs from Main function call
+## All test cases passed for assessment! 
+
+# Modified employee list: ['John_Kitchen', 'Paul_House Floor', 'Sarah_Management', 'Lisa_Cold Storage', 'Ryan_Inventory Mgmt', 'Gill_Cashier']
+
+# List of usernames: ['John_Kitchen', 'Paul_House_Floor', 'Sarah_Management', 'Lisa_Cold_Storage', 'Ryan_Inventory_Mgmt', 'Gill_Cashier']
+
+# Initials and ids: {'J': 12345, 'P': 12456, 'S': 12478, 'L': 12434, 'R': 12483, 'G': 12419}
